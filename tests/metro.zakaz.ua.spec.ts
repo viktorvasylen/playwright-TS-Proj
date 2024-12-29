b2672aa2f4d0226433ff01e169d91346b5f43be7
viktorvasylen <vasylenko.viktor@gmail.com>
2024-12-29 20:24:47 +0200

    Rename test from 'has title' to 'has URL' in metro.zakaz.ua.spec.ts

--- tests/metro.zakaz.ua.spec.ts
+++ tests/metro.zakaz.ua.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
+import { isContext } from 'vm';
 
-test('has title', async ({ page }) => {
+test('has URL', async ({ page }) => {
   await page.goto('https://metro.zakaz.ua/');
 
   // Expect a title "to contain" a substring.
